1bae29149428a0d3b185d365209e10d85986b846
N.Shimizu <chikoski@gmail.com>
2015-12-03 09:25:14 +0900

    initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function initialize(){
+  var hiddenMessageElement = document.querySelector("#hidden-message");
+  hiddenMessageElement.hidden = true;
+}
+
+window.addEventListener("load", initialize);
